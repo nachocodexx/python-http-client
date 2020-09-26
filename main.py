0d029eb3f9dc1fce7fcba244b65091367a4e71f9
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2020-09-26 18:38:06 -0500

    [updated]:fire:

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('www.google.com', 80)
 client_socket.connect(server_address)
 
-request_header = 'GET / HTTP/1.0\r\nHost: www.google.com\r\n\r\n'
+request_header = 'GET / HTTP/1.0\r\nHost: www.google.com\r\n\r\n'.encode()
 client_socket.send(request_header)
 
 response = ''
@@ -12,7 +12,7 @@ while True:
     recv = client_socket.recv(1024)
     if not recv:
         break
-    response += recv
+    response += str(recv)
 
 print(response)
 client_socket.close()
